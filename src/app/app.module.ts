a2b4823cbe7a9a1728f92b139cc8c2d181282ac1
hiroxtai <44028477+hiroxtai@users.noreply.github.com>
2019-05-10 00:57:22 +0900

    HeroesComponent を宣言する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+// 自作したコンポーネントを使用するためにインポートする
 import { HeroesComponent } from './heroes/heroes.component';
 
 // [(ngModel)] の双方向データバインディング構文を使用するためにインポートする
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
+  // モジュールに属するコンポーネントを登録する
   declarations: [
     AppComponent,
     HeroesComponent
