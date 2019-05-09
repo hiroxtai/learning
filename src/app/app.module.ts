4503cc5da08f9d182657359d382e26c36a6bd03d
hiroxtai <44028477+hiroxtai@users.noreply.github.com>
2019-05-10 00:49:04 +0900

    ヒーローを編集する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+// [(ngModel)] の双方向データバインディング構文を使用するためにインポートする
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
+  // アプリケーションに必要な外部モジュールのリストを記述する
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
